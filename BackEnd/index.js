dec6cabcad62ace32f85dfd1275e9f8bff3db8cf
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-05-13 13:21:34 +0530

    c

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-        origin: 'https://stalwart-daifuku-f33a84.netlify.app/',
+        origin: 'https://stalwart-daifuku-f33a84.netlify.app',
         credentials: true 
         }))
 
